c9f1e3f3b1f2a0195fc619da5e9b15979cb2ba9c
camille <c.miyashita@gmail.com>
2022-11-23 16:55:34 -0300

    html e css prontos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 
 
-function App() {
-  return (
-   <>
-   <h1>hello</h1>
-   </>
-  );
-}
+// function App() {
+//   return (
+//    <>
+//    <h1>hello</h1>
+//    </>
+//   );
+// }
 
-export default App;
+// export default App;
